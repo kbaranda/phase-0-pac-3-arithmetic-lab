8f400dcb617f30ce669b89e9f169483721cba3df
Kat <bolivarkat@yahoo.com>
2021-08-13 20:05:17 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+number = 10;
+
+function add() {
+    1 + 80;
+}
+function subtract() {
+    60 - 40;
+}
+function multiply() {
+    2 * 3.4;
+}
+function divide() {
+    5.0 / 2.5;
+}
+a = 5;
+b = 2;
+function add(a,b) {
+    return a += b;
+}
+function subtract(a, b) {
+    return a -= b;
+}
+function multiply(a, b) {
+    return a *= b;
+}
+function divide(a, b) {
+    return a /= b;
+}
+function increment(n) {
+   return n += 1;
+}
+function decrement(n) {
+    return n -= 1;
+} 
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
